82a6d702cccce4c85f8c881397a8eede73e9b797
Uyeb <uyenruby17@gmail.com>
2025-06-11 11:09:39 +0700

    hehe

--- config.js
+++ config.js
@@ -2,7 +2,7 @@ const CONFIG = {
     introTitle: 'anh à!',
     introDesc: `Trái đất vốn lạ thường
     Mà sao anh cứ đi nhầm đường
-    Lạc vào tim em lẻ loi
+    Lạc vào tim em 
     Đằng sau chữ yêu đây là thương`,
     btnIntro: 'hihi',
     title: 'Phải chăng anh đã yêu ngay từ cái nhìn đầu tiên 😙',
@@ -12,9 +12,9 @@ const CONFIG = {
     question:'Trên thế giới hơn 7 tỉ người mà sao anh lại yêu em <3',
     btnReply: 'Gửi cho em <3',
     reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'em biết mà 🥰. không yêu anh nhiều nhiều 😘😘',
-    messDesc: 'cho em xin 2 nghìn nhá .',
+    mess: 'em biết mà 🥰. yêu anh nhiều nhiều 😘😘',
+    messDesc: 'cho em xin 1 triệu nhá 😘😘.',
     btnAccept: 'Okiiiii lun <3',
-    messLink: 'https://www.facebook.com //link mess của các bạn. VD: https://m.me/nam.nodemy
+    messLink: 'img/qr.jpg'
 }
 
--- js/main.js
+++ js/main.js
@@ -1,133 +1,177 @@
-$(document).ready(function() {
-    // process bar
-    setTimeout(function() {
-        firstQuestion();
-        $('.spinner').fadeOut();
-        $('#preloader').delay(350).fadeOut('slow');
-        $('body').delay(350).css({
-            'overflow': 'visible'
-        });
-    }, 600);
-})
+$(document).ready(function () {
+  // process bar
+  setTimeout(function () {
+    firstQuestion();
+    $(".spinner").fadeOut();
+    $("#preloader").delay(350).fadeOut("slow");
+    $("body").delay(350).css({
+      overflow: "visible",
+    });
+  }, 600);
+});
 
-function init(){
-    $('#title').text(CONFIG.title)
-    $('#desc').text(CONFIG.desc)
-    $('#yes').text(CONFIG.btnYes)
-    $('#no').text(CONFIG.btnNo)
+function init() {
+  $("#title").text(CONFIG.title);
+  $("#desc").text(CONFIG.desc);
+  $("#yes").text(CONFIG.btnYes);
+  $("#no").text(CONFIG.btnNo);
 }
 
-function firstQuestion(){
-    $('.content').hide();
-    Swal.fire({
-        title: CONFIG.introTitle,
-        text: CONFIG.introDesc,
-        imageUrl: 'img/logi.gif',
-        imageWidth: 300,
-        imageHeight: 300,
-        background: '#fff url("img/iput-bg.jpg")',
-        imageAlt: 'Custom image',
-        confirmButtonText: CONFIG.btnIntro
-      }).then(function(){
-        $('.content').show(200);
-      })
+function firstQuestion() {
+  $(".content").hide();
+  Swal.fire({
+    title: CONFIG.introTitle,
+    text: CONFIG.introDesc,
+    imageUrl: "img/logi.gif",
+    imageWidth: 300,
+    imageHeight: 300,
+    background: '#fff url("img/iput-bg.jpg")',
+    imageAlt: "Custom image",
+    confirmButtonText: CONFIG.btnIntro,
+  }).then(function () {
+    $(".content").show(200);
+  });
 }
 
- // switch button position
- function switchButton() {
-    var audio = new Audio('sound/duck.mp3');
-    audio.play();
-    var leftNo = $('#no').css("left");
-    var topNO = $('#no').css("top");
-    var leftY = $('#yes').css("left");
-    var topY = $('#yes').css("top");
-    $('#no').css("left", leftY);
-    $('#no').css("top", topY);
-    $('#yes').css("left", leftNo);
-    $('#yes').css("top", topNO);
+// switch button position
+function switchButton() {
+  var audio = new Audio("sound/duck.mp3");
+  audio.play();
+  var leftNo = $("#no").css("left");
+  var topNO = $("#no").css("top");
+  var leftY = $("#yes").css("left");
+  var topY = $("#yes").css("top");
+  $("#no").css("left", leftY);
+  $("#no").css("top", topY);
+  $("#yes").css("left", leftNo);
+  $("#yes").css("top", topNO);
 }
 // move random button póition
 function moveButton() {
-    var audio = new Audio('sound/Swish1.mp3');
-    audio.play();
-    var x = Math.random() * ($(window).width() - $('#no').width()) * 0.9 ;
-    var y = Math.random() * ($(window).height() - $('#no').height()) * 0.9;
-    var left = x + 'px';
-    var top = y + 'px';
-    $('#no').css("left", left);
-    $('#no').css("top", top);
+  var audio = new Audio("sound/Swish1.mp3");
+  audio.play();
+  var x = Math.random() * ($(window).width() - $("#no").width()) * 0.9;
+  var y = Math.random() * ($(window).height() - $("#no").height()) * 0.9;
+  var left = x + "px";
+  var top = y + "px";
+  $("#no").css("left", left);
+  $("#no").css("top", top);
 }
 
-init()
+init();
 
 var n = 0;
-$('#no').mousemove(function() {
-    if (n < 1)
-        switchButton();
-    if (n > 1)
-        moveButton();
-    n++;
+$("#no").mousemove(function () {
+  if (n < 1) switchButton();
+  if (n > 1) moveButton();
+  n++;
 });
-$('#no').click(() => {
-    if (screen.width>=900)
-        switchButton();
-})
+$("#no").click(() => {
+  if (screen.width >= 900) switchButton();
+});
 
 // generate text in input
 function textGenerate() {
-    var n = "";
-    var text = " " + CONFIG.reply;
-    var a = Array.from(text);
-    var textVal = $('#txtReason').val() ? $('#txtReason').val() : "";
-    var count = textVal.length;
-    if (count > 0) {
-        for (let i = 1; i <= count; i++) {
-            n = n + a[i];
-            if (i == text.length + 1) {
-                $('#txtReason').val("");
-                n = "";
-                break;
-            }
-        }
+  var n = "";
+  var text = " " + CONFIG.reply;
+  var a = Array.from(text);
+  var textVal = $("#txtReason").val() ? $("#txtReason").val() : "";
+  var count = textVal.length;
+  if (count > 0) {
+    for (let i = 1; i <= count; i++) {
+      n = n + a[i];
+      if (i == text.length + 1) {
+        $("#txtReason").val("");
+        n = "";
+        break;
+      }
     }
-    $('#txtReason').val(n);
-    setTimeout("textGenerate()", 1);
+  }
+  $("#txtReason").val(n);
+  setTimeout("textGenerate()", 1);
 }
 
 // show popup
-$('#yes').click(function() {
-    var audio = new Audio('sound/tick.mp3');
-    audio.play();
-    Swal.fire({
-        title: CONFIG.question,
-        html: true,
-        width: 900,
-        padding: '3em',
-        html: "<input type='text' class='form-control' id='txtReason' onmousemove=textGenerate()  placeholder='Whyyy'>",
-        background: '#fff url("img/iput-bg.jpg")',
-        backdrop: `
+$("#yes").click(function () {
+  var audio = new Audio("sound/tick.mp3");
+  audio.play();
+  Swal.fire({
+    title: CONFIG.question,
+    html: true,
+    width: 900,
+    padding: "3em",
+    html: "<input type='text' class='form-control' id='txtReason' onmousemove=textGenerate()  placeholder='Whyyy'>",
+    background: '#fff url("img/iput-bg.jpg")',
+    backdrop: `
               rgba(0,0,123,0.4)
               url("img/giphy2.gif")
               left top
               no-repeat
             `,
-        confirmButtonColor: '#3085d6',
-        confirmButtonColor: '#fe8a71',
-        confirmButtonText: CONFIG.btnReply
-    }).then((result) => {
-        if (result.value) {
-            Swal.fire({
-                width: 900,
-                confirmButtonText: CONFIG.btnAccept,
-                background: '#fff url("img/iput-bg.jpg")',
-                title: CONFIG.mess,
-                text: CONFIG.messDesc,
-                confirmButtonColor: '#83d0c9',
-                onClose: () => {
-                    window.location = CONFIG.messLink;
-                  }
-            })
-        }
-    })
-})
+    confirmButtonColor: "#3085d6",
+    confirmButtonColor: "#fe8a71",
+    confirmButtonText: CONFIG.btnReply,
+  }).then((result) => {
+    if (result.value) {
+      // 💥 Thêm pháo hoa tung bay tại đây:
+      confetti({
+        particleCount: 150,
+        angle: 70,
+        spread: 75,
+        origin: { x: 0, y: 0.5 }, // Từ trái
+      });
+      confetti({
+        particleCount: 150,
+        angle: 120,
+        spread: 75,
+        origin: { x: 1, y: 0.5 }, // Từ phải
+      });
 
+      Swal.fire({
+        width: 900,
+        title: CONFIG.mess,
+        html: `
+                    <p>${CONFIG.messDesc}</p>
+                    <img src="${CONFIG.messLink}" width="300" height="300" alt="QR Code">
+                `,
+        confirmButtonText: CONFIG.btnAccept,
+        background: '#fff url("img/iput-bg.jpg")',
+        confirmButtonColor: "#83d0c9",
+      })
+      .then(() => {
+    // Bắn pháo
+    confetti({
+        particleCount: 100,
+        angle: 60,
+        spread: 55,
+        origin: { x: 0, y: 0.5 }
+    });
+    confetti({
+        particleCount: 100,
+        angle: 120,
+        spread: 55,
+        origin: { x: 1, y: 0.5 }
+    });
+
+    // Hiện ảnh GIF hoặc video mới
+    Swal.fire({
+        title: 'Tặng em nè 💖',
+        html: `<img src="img/logi.gif" width="300" height="300" alt="Cute GIF">`, // Đường dẫn ảnh/gif
+        background: '#fff',
+        showConfirmButton: false,
+        timer: 4000 // tự tắt sau 4s
+    });
+});
+    }
+  });
+});
+
+document.addEventListener(
+  "click",
+  function () {
+    var audio = document.getElementById("bgMusic");
+    audio.muted = false;
+    audio.play();
+  },
+  { once: true }
+);
